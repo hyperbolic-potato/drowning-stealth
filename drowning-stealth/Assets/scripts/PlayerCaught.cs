0e0f5e934363d0fc9a0a16f27806478cc02c7f38
hyperbolic-potato <152937040+hyperbolic-potato@users.noreply.github.com>
2025-11-19 13:23:40 -0800

    Added a slightly scuffed checkpoint system

--- drowning-stealth/Assets/scripts/PlayerCaught.cs
+++ drowning-stealth/Assets/scripts/PlayerCaught.cs
@@ -3,11 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class PlayerCaught : MonoBehaviour
 {
+    public Vector2 respawnPosition;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if(GameObject.FindGameObjectsWithTag("Player").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Checkpoint"))
+        {
+            respawnPosition = collision.transform.position;
+            Destroy(collision.gameObject);
+        }        
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.CompareTag("Enemy"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // a stop-gap solution until checkpoints are implemented
+            transform.position = respawnPosition;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
