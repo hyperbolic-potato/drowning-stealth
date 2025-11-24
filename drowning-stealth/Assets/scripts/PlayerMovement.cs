231bd16b3b575fe02ffdf76ed39dedd4f803d41a
hyperbolic-potato <152937040+hyperbolic-potato@users.noreply.github.com>
2025-11-24 13:20:14 -0800

    finished up lvl 1. working on animation stuff

--- drowning-stealth/Assets/scripts/PlayerMovement.cs
+++ drowning-stealth/Assets/scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -19,6 +21,12 @@ public class PlayerMovement : MonoBehaviour
     public bool isSprinting;
     public bool isCrawling;
 
+    public bool isTrapped;
+
+    public int escapeThreshold;
+    public int escapeProgress;
+    public SpriteRenderer escapeIndicator;
+
     public float moveNoise = 1.5f;
     public float crawlNoise = 0.75f;
     public float sprintNoise = 3f;
@@ -33,54 +41,76 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         anim = GetComponent<Animator>();
+
+        escapeIndicator = transform.GetChild(2).GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        //movement
-        Vector2 move = Vector2.zero;
-        if (isMoving)
+        if (isTrapped)
         {
-            if (isCrawling)
+            if (escapeProgress >= escapeThreshold)
             {
-                move = moveInput * crawlSpeed;
-                noise = crawlNoise;
+                isTrapped = false;
             }
-            else if (isSprinting)
-            {
-                move = moveInput * sprintSpeed;
-                noise = sprintNoise;
-            }
-            else
-            {
-                move = moveInput * moveSpeed;
-                noise = moveNoise;
-            }                    
-
-            
+            escapeIndicator.enabled = true;
+            escapeIndicator.size = new Vector2((float)escapeProgress / (float)escapeThreshold, 1);
+            escapeIndicator.transform.localPosition = new Vector3(-0.5f * (1 - (float)escapeProgress / (float)escapeThreshold), 0.6f, 0);
         }
         else
         {
-            noise = idleNoise;
+            escapeIndicator.enabled = false;
         }
 
+            //movement
+            Vector2 move = Vector2.zero;
+        if(!isTrapped)
+        {
+            if (isMoving)
+            {
+                if (isCrawling)
+                {
+                    move = moveInput * crawlSpeed;
+                    noise = crawlNoise;
+                }
+                else if (isSprinting)
+                {
+                    move = moveInput * sprintSpeed;
+                    noise = sprintNoise;
+                }
+                else
+                {
+                    move = moveInput * moveSpeed;
+                    noise = moveNoise;
+                }
 
 
-        if (move.x < 0)         nesw = 4;
-        else if (move.x > 0)    nesw = 2;
-        else if (move.y > 0)    nesw = 1;
-        else if (move.y < 0)    nesw = 3;
-        //else                    nesw = 0;
+            }
+            else
+            {
+                noise = idleNoise;
+            }
+
+
+
+            if (move.x < 0) nesw = 4;
+            else if (move.x > 0) nesw = 2;
+            else if (move.y > 0) nesw = 1;
+            else if (move.y < 0) nesw = 3;
+            //else                    nesw = 0;
 
 
 
             rb.linearVelocity = move;
 
 
+            
+        }
         anim.SetBool("isMoving", isMoving);
         anim.SetBool("isCrouching", isCrawling);
         anim.SetBool("isSprinting", isSprinting);
         anim.SetInteger("NESW", nesw);
+
 
     }
 
@@ -98,7 +128,30 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
-        isMoving = context.ReadValue<Vector2>().magnitude > 0;
+        if (!isTrapped)
+        {
+            moveInput = context.ReadValue<Vector2>();
+            isMoving = context.ReadValue<Vector2>().magnitude > 0;
+        }
+        else if ( Random.Range(0f, 1f) <= 0.4f && context.performed)
+        {
+            escapeProgress++;
+        }
+        
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Sinkhole"))
+        {
+            isTrapped = true;
+            rb.linearVelocity = Vector2.zero;
+            isMoving = false;
+            escapeProgress = 0;
+
+            collision.enabled = false;
+        }
+    }
+
+
 }
